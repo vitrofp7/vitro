f7fec9d3056db7da12ed47a81d7ecaf33187bc0f
antoniou79 <antoniou@cti.gr>
2013-07-04 15:59:20 +0300

    Moved gateway module one folder up. Removed TODO file for pending revising.

--- source/trunk/Demo/vitro-gateway/vitro-gateway/ws4d/coap/interfaces/CoapChannelListener.java
+++
@@ -1,9 +0,0 @@
-
-package org.ws4d.coap.interfaces;
-/**
- * @author Christian Lerche <christian.lerche@uni-rostock.de>
- */
-
-public interface CoapChannelListener {
-	
-}
--- source/trunk/Demo/vitro-gateway/vitro-gateway/ws4d/coap/rest/ResourceHandler.java
+++
@@ -1,9 +0,0 @@
-package org.ws4d.coap.rest;
-
-/**
- * @author Christian Lerche <christian.lerche@uni-rostock.de>
- */
-
-public interface ResourceHandler {
-		public void onPost(byte[] data);
-}
