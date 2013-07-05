d36dc2a04d335dda4356bca4fc1ccb7842b09a3e
Paolo <paolomeda@gmail.com>
2013-07-05 11:00:33 +0200

    updated licence of gw

--- source/trunk/Demo/vitro-gateway/src/main/java/org/ws4d/coap/rest/CoapResourceServer.java
+++ source/trunk/Demo/vitro-gateway/src/main/java/org/ws4d/coap/rest/CoapResourceServer.java
@@ -1,21 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2013 VITRO FP7 Consortium.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser Public License v3.0 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl-3.0.html
- * 
- * Contributors:
- *     Antoniou Thanasis
- *     Paolo Medagliani
- *     D. Davide Lamanna
- *     Panos Trakadas
- *     Andrea Kropp
- *     Kiriakos Georgouleas
- *     Panagiotis Karkazis
- *     David Ferrer Figueroa
- *     Francesco Ficarola
- *     Stefano Puglia
- ******************************************************************************/
 package org.ws4d.coap.rest;
 
 import java.net.InetAddress;
